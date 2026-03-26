f4c33a95b640d7d4933e099cd0eee78c203d8484
krishnabakde-design <krishna.bakde@avantika.edu.in>
2026-03-26 08:03:21 +0000

    Next Task Done

--- test.py
+++ test.py
@@ -8,7 +8,7 @@
 T = int(input())
 for i in range(T):
     X = int(input())
-    if 10000 > X >= 2000:
+    if X >= 2000 and X < 10000:
         print("Yes")
     elif X >= 10000:
         print("It's Too Much")
